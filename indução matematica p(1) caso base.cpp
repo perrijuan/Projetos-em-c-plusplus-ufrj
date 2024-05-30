1df58b32cbdb0b5f7414a6e7dd2deb490ce15604
Juan perri <111884460+Xorjuan@users.noreply.github.com>
2024-05-30 15:06:04 -0300

    Create indução matematica p(1) caso base.cpp

---
+++ indução matematica p(1) caso base.cpp
@@ -0,0 +1,12 @@
+//Mostre que, se n é um inteiro positivo:
+//indução matematica usado o caso base p(1):
+
+#include <stdio.h>
+
+int main()
+{
+    int a=1;
+    int ans=1*(1+1)/2;
+    printf("%d", ans);
+    return 0;
+}

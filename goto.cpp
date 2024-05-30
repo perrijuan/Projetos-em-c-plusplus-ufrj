75d0f96230a59172d01800c41d8e061c5c1a65fb
Juan perri <111884460+Xorjuan@users.noreply.github.com>
2024-05-30 14:28:15 -0300

    Add files via upload
    uso de goto para um algortimo simples, sendo altamente desaconselhavel uso pois com a tese o dijkstra em seu em 1960, ele diz que a manutenção desse codigo em baixo nivel tem uma maior dificuldade visto que o goto pula as instruções logo no desasembly isso gera uma alta complexidade da busca por erros.+

--- goto.cpp
+++ goto.cpp
@@ -1,23 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
- int main(){
-     vector<int> a{};
-     for(int i=0;i<10;i++){
-         int aux;
-         cin >> aux;
-         a.push_back(aux);
-     }
-     int b;
-     cin>>b;
-     for(int y=0;y<10;y++){
-         if(a[y]==b){
-             cout<<"SIM";
-             goto label;
-         }
-     }
-     
-     cout<<"NÃO";
-     label:
-     return 0;
- }
+int main() {
+  vector<int> a{};
+  for (int i = 0; i < 10; i++) {
+    int aux;
+    cin >> aux;
+    a.push_back(aux);
+  }
+  int b;
+  cin >> b;
+  for (int y = 0; y < 10; y++) {
+    if (a[y] == b) {
+      cout << "SIM";
+      goto label;
+    }
+  }
+
+  cout << "NÃO";
+label:
+  return 0;
+}

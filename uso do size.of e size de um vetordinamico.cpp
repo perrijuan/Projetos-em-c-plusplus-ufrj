b1148e2be7ac34ce3d7ace921b66aec3b479751e
Juan perri <111884460+Xorjuan@users.noreply.github.com>
2024-06-02 12:13:07 -0300

    Create uso do size.of e size de um vetordinamico.cpp

---
+++ uso do size.of e size de um vetordinamico.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//strings são vetores dinamicos logo não é necessario o uso de um vetor em um caso base
+int main()
+{
+    
+   string s;
+   
+   cin>>s;
+   
+   int ans =s.size();
+   
+   cout<<ans;   
+ 
+    return 0;
+}

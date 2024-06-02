0a66f7b122cc6a2f2892d58a117221b15e680bc3
Juan perri <111884460+Xorjuan@users.noreply.github.com>
2024-06-02 12:09:32 -0300

    Create Móbile (OBI 2015)_neps50.cpp
    estruturas de decisão e equações

---
+++ Móbile (OBI 2015)_neps50.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    
+   int a,b,c,d;
+   
+   cin>>a>>b>>c>>d;
+   
+   if(a==b+c+d&&d==b+c&&b==c){
+       cout<<"s";
+   }else{
+       cout<<"n";
+   }
+    
+
+    return 0;
+}
